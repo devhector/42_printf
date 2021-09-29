3a60d3a53fe261be1b509880c884553d723fe1c2
Hector Fernandes <hectorhu17@gmail.com>
2021-09-29 20:35:41 -0300

    add func utoa

---
+++ libft/ft_utoa.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 20:33:45 by hectfern          #+#    #+#             */
+/*   Updated: 2021/09/29 20:34:22 by hectfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_utoa(unsigned int n)
+{
+	char			*p;
+	int				size;
+	unsigned int	x;
+
+	x = n;
+	size = 0;
+	while (x > 10)
+	{
+		x /= 10;
+		size++;
+	}
+	p = (char *)malloc(sizeof(p) * (size + 1));
+	if (p)
+	{
+		p[size + 1] = '\0';
+		while (size >= 0)
+		{
+			x = n % 10;
+			p[size] = 48 + x;
+			n = n / 10;
+			size--;
+		}
+	}
+	return (p);
+}
