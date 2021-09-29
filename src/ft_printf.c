15a57edb5bb079ac3471f18b4a3ff3fd1ba667ff
Hector Fernandes <hectorhu17@gmail.com>
2021-09-29 17:40:40 -0300

    add "d" param

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,6 +1,38 @@
-#include "libftprintf.h"
-#include "libft.h"
-#include <stdarg.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 16:03:59 by hectfern          #+#    #+#             */
+/*   Updated: 2021/09/29 17:40:15 by hectfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libftprintf.h"
+
+int	ft_printf_arg(const char *fmt, int i, va_list ap);
+
+int ft_putchar(char c)
+{
+	write (1, &c, 1);
+	return (1);
+}
+
+int ft_putstr(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	write (1, str, len);
+	return (len);
+}
+
+int ft_putnbr(int nbr)
+{
+	return (ft_putstr(ft_itoa(nbr)));
+}
 
 int ft_printf(const char *format, ...)
 {
@@ -13,11 +45,11 @@ int ft_printf(const char *format, ...)
 	va_start(ap, format);
 	while (format[i])
 	{
-		if (format[i] == '%')
-			if (ft_strchr("cspdiuxX%", format[i + 1]))
-				ret += ft_printf_arg(format, &i, ap);
-			else
-				ret += ft_putchar(format[i]);
+		if (format[i] == '%' && ft_strchr("cspdiuxX%", format[i + 1]))
+		{
+			ret += ft_printf_arg(format, i, ap);
+			i++;
+		}
 		else
 			ret += ft_putchar(format[i]);
 		i++;
@@ -27,9 +59,15 @@ int ft_printf(const char *format, ...)
 	return (ret);
 }
 
-int	ft_printf_arg(const char *fmt, int *i, va_list ap)
+int	ft_printf_arg(const char *fmt, int i, va_list ap)
 {
-	if (fmt[*i + 1] == "%")
-		return (ft_putchar(fmt[*i + 1]));
-	return 0;	 
+	if (fmt[i + 1] == '%')
+		return (ft_putchar(fmt[i + 1]));
+	else if (fmt[i + 1] == 'c')
+		return (ft_putchar(va_arg(ap, int)));
+	else if (fmt[i + 1] == 's')
+		return (ft_putstr(va_arg(ap, char *)));
+	else if (fmt[i + 1] == 'd' || fmt[i + 1] == 'i')
+		return (ft_putnbr(va_arg(ap, int)));
+	return (0);	 
 }
