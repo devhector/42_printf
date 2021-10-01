2665f34b0d69f60d3c0c0378c726f5a27fbde58f
Hector Fernandes <hectorhu17@gmail.com>
2021-10-01 18:48:47 -0300

    edit header file

--- include/libftprintf.h
+++ include/libftprintf.h
@@ -5,7 +5,11 @@
 #include <unistd.h>
 #include "../libft/libft.h"
 
-char	*ft_strchr(const char	*s, int	c);
-int		ft_printf(const char	*format, ...);
+int	ft_putchar(char c);
+int	ft_putnbr(int nbr);
+int	ft_putstr(char *str);
+int	putnbr_u(unsigned int nbr);
+int	puthex(unsigned long nbr, char *base);
+int	ft_printf(const char	*format, ...);
 
 #endif
